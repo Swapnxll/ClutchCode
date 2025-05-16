28f5871bca47fd5783cb7a72b74c6eb9cad8c471
Swapnxll <130583218+Swapnxll@users.noreply.github.com>
2025-05-17 01:43:42 +0530

    backend done

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,24 @@ import { connectDb } from "./config/db.js";
 import userRoute from "./routes/userRoute.js";
 import courseRoute from "./routes/courseRoute.js";
 import adminRoute from "./routes/adminRoute.js";
+import Razorpay from "razorpay";
+import cors from "cors";
+
 dotenv.config();
+
+export const instance = new Razorpay({
+  key_id: process.env.Razorpay_Key,
+  key_secret: process.env.Razorpay_Secret,
+});
+
 const PORT = process.env.PORT || 8081;
 const app = express();
+app.use(cors());
+
 dotenv.config();
 //middleware
 app.use(express.json());
+app.use("/uploads", express.static("uploads"));
 
 //routes
 app.get("/", (req, res) => {
@@ -18,9 +30,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api", userRoute);
-
 app.use("/api", courseRoute);
 app.use("/api", adminRoute);
+
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`.bgYellow);
   connectDb();
